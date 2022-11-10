32d6d3da149cf498ce3eb7e0b73562c681652d24
AndreaLM06 <andrea.larboullet---marin@etu.unice.fr>
2022-11-10 20:55:05 +0100

    commit

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-j=jaz-i%^mb9arxmlj*a5zr%8kzos)j((6vr0_+a9rvu8o!4nv'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
